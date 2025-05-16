038aaaae295eb28e1eb2c6a554cd39c9a09ad394
Greg Richardson <greg.nmr@gmail.com>
2025-05-16 14:05:03 -0600

    chore: run unit tests over multiple runtimes

--- packages/pg-parser/vitest.workspace.js
+++ packages/pg-parser/vitest.workspace.js
@@ -8,4 +8,28 @@ export default defineWorkspace([
       include: ['src/**/*.{test,spec}.ts'],
     },
   },
+  {
+    test: {
+      name: 'unit:vercel-edge',
+      environment: 'edge-runtime',
+      include: ['src/**/*.{test,spec}.ts'],
+    },
+  },
+  {
+    test: {
+      name: 'unit:browser',
+      include: ['src/**/*.{test,spec}.ts'],
+      browser: {
+        enabled: true,
+        provider: 'playwright',
+        headless: true,
+        screenshotFailures: false,
+        instances: [
+          { browser: 'chromium' },
+          { browser: 'firefox' },
+          { browser: 'webkit' },
+        ],
+      },
+    },
+  },
 ]);
